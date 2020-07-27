e0322973fae4ca847bb4b03c296c1bc3767de412
Markus Boeck <e1634838@student.tuwien.ac.at>
2020-07-27 15:47:53 +0200

    play game

--- MuBetaZero.jl
+++ MuBetaZero.jl
@@ -1,10 +1,84 @@
 include("Tree.jl")
+include("utils.jl")
+include("TikTakToe.jl")
 
-struct MuBetaZero
+abstract type MuBetaZero end
+
+mutable struct MuBetaZeroTabular <: MuBetaZero
     Q::Vector{Float32}
     γ::Float32
+    α::Float32
+    ϵ::Float64
+
+    function MuBetaZeroTabular(n_states, n_actions; γ=0.99, α=0.1, ϵ=0.1, init=:zero)
+        this = new()
+        if init == :zero
+            this.Q = zeros(Float32, n_states * n_actions)
+        elseif init == :random
+            this.Q = rand(Float32, n_states * n_actions) * 2 .- 1
+        end
+        this.γ = γ
+        this.α = α
+        this.ϵ = ϵ
+        return this
+    end
 end
 
+function greedy_action(μβ0::MuBetaZeroTabular, env::Environment, state::Vector{Int})
+    i = s_a_to_index(env, state, 0)
+    is = [i + j for j in 1:env.n_actions] # TODO: assertion
+    a, Q = maximise(a -> μβ0.Q[is[a]], valid_actions(env, state))
+    return a, Q
+end
+
+# TODO: split from update
+function play!(μβ0::MuBetaZeroTabular, env::Environment, adversary::MuBetaZeroTabular)
+    s = env.current
+
+    if rand() ≤ μβ0.ϵ
+        a = rand(valid_actions(env, s))
+    else
+        a, = greedy_action(μβ0, env, s) # TODO: MCTS
+    end
+
+    i = s_a_to_index(env, s, a)
+
+    print_current(env)
+    r1, done, = step!(env, a, false)
+    print_current(env)
+    r2 = 0f0
+    if !done
+        a_adv, = greedy_action(adversary, env, env.current)
+        r2, done = step!(env, a_adv, true)
+        print_current(env)
+    end
+    r = r1 + r2
+
+    α = μβ0.α; γ = μβ0.γ
+    println(r, ", ", done)
+
+    if !done
+        a_star, Q_star = greedy_action(μβ0, env, env.current)
+    else
+        Q_star = 0f0
+    end
+
+    # μβ0.Q[i] = (1 - α) * μβ0.Q[i] + α * (r + γ * Q_star)
+
+    return done
+end
+
+function play_game!(μβ0::MuBetaZeroTabular, env::Environment, adversary::MuBetaZeroTabular)
+    reset!(env)
+    done = false
+    while !done
+        done = play!(μβ0, env, adversary)
+    end
+end
+
+# function update!(μβ0::MuBetaZeroTabular, env::Environment, s::Int, a::Int, r::Float32)
+
+# TODO test MCTS without rollout
 function MCTS(μβ0::MuBetaZero, env::Environment, N::Int; expand_at=1)
     state = copy(env.current)
     root = MCTSNode()
@@ -70,7 +144,7 @@ function rollout!(μβ0::MuBetaZero, env::Environment, foe::Bool)
     r = 0.0f0
     done = false
     while !done
-        action = rand(valid_actions(env.current)) #greedy(μβ0, env.current)
+        action = greedy_action(μβ0, env, env.current) # self play
         r, done = step!(env, action, foe)
     end
     return r
@@ -85,8 +159,16 @@ function backpropagate!(node::MCTSNode, r::Float32)
     end
 end
 
-μβ0 = MuBetaZero([], 1.0)
 env = TikTakToe()
+reset!(env)
+μβ0 = MuBetaZeroTabular(env.n_states, env.n_actions)
+using Random
+Random.seed!(1)
+adv = MuBetaZeroTabular(env.n_states, env.n_actions, init=:random)
+
+greedy_action(μβ0, env, env.current)
+
+play_game!(μβ0, env, adv)
 
 A = [
  0 0 2;
@@ -98,12 +180,3 @@ env.current = reshape(A, :)
 print_current(env)
 
 MCTS(μβ0, env, 1_000_000)
-
-tree = MCTSTree(MCTSNode())
-push!(tree.root.children, MCTSNode(tree.root, 1))
-
-println(tree)
-
-leaf = tree.root.children[1]
-
-print_tree(tree)
